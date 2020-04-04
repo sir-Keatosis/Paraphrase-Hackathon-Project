2d0864b33b4b323cb4fa0e85fd69b86660c100c6
brainsincluded <45725627+brainsincluded@users.noreply.github.com>
2020-04-04 11:30:29 -0700

    Update FileFuncs.cpp

---
+++ Paraphrase-Hackathon-Project/FileFuncs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "CounterManager.h"
+#include "TextParser.h"
+#include <cstdlib>
+#include <filesystem>
+#include <string>
+#include <cmath>
+
+bool isRightType(std::string filePath)
+{
+    std::string path = filePath;
+    int length;
+
+    //loop removing all bt last for chars
+    //return (path == ".txt" ? true : false );
+    return true;
+}
+
+bool isValidFilePath(std::string fIlePath)
+{
+    //
+    //create path instance
+    //return result of checking method
+    return true;
+}
+
+bool
+
+
+
+
+
+
