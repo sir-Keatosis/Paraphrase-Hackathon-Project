c1de1578a51222a14743c986078523cb421465a2
sir-Keatosis <62768320+sir-Keatosis@users.noreply.github.com>
2020-04-04 14:29:31 -0700

    final commit before lunch, missed some stuff with dividing that has been cleared up

--- Paraphrase-Hackathon-Project/TextParser.cpp
+++ Paraphrase-Hackathon-Project/TextParser.cpp
@@ -210,6 +210,17 @@ void TextParser::modify_counter(std::string* unParse, int stringPos)
 			}
 			counters.number_counter_multiply(variable, value_mod);
 			break;
+		case'/':
+			value_mod = 0; //new temporary variable that holds the value that the number counter is being divided by
+			int iterations = 0; //new temporary variable that can handle multiple digits
+			while (unParse->at(stringPos + offset) != ' ' and unParse->at(stringPos + offset) != '|')
+			{
+				value_mod = (value_mod * iterations) + ((unParse->at(stringPos + offset) - '0')); //gonna need to do some check within stuff here for multi variable maths
+				iterations++;
+				offset++;
+			}
+			counters.number_counter_divide(variable, value_mod);
+			break;
 		default:
 			variable += unParse->at(stringPos + offset);
 			break;
