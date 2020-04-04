566b9afc5618f7452b91026cd766a1c862de1359
brainsincluded <45725627+brainsincluded@users.noreply.github.com>
2020-04-04 15:40:56 -0700

    Update FileFuncs.cpp

--- Paraphrase-Hackathon-Project/FileFuncs.cpp
+++ Paraphrase-Hackathon-Project/FileFuncs.cpp
@@ -21,6 +21,38 @@ bool isValidFile(std::string filePath, std::fstream stream)
     return stream.good();
 }
 
+std::string readFile(std::ifstream stream, std::string file)
+{
+    const std::string failStmnt = "Sorry, unable to open file";
+
+    if (isRightType(file) == true)
+    {
+        stream.open(file);
+        if (stream.good() == true)
+        {
+            {
+                char* str;
+                std::string output = "";
+                while (stream.get(str, 5000))
+                {
+                    output.append(str);
+                }
+                stream.close();
+                return output;
+            }
+        }
+        else
+        {
+            return failStmnt;
+        }
+
+
+    }
+    else
+    {
+        return failStmnt;
+    }
+}
 
 
 
