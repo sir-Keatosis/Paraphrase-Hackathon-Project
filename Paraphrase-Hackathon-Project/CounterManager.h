9e6726848464180023f14aacfbf065817e71df30
sir-Keatosis <62768320+sir-Keatosis@users.noreply.github.com>
2020-04-03 22:51:43 -0700

    Started the counter manager

---
+++ Paraphrase-Hackathon-Project/CounterManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<map>
+class CounterManager
+{
+public:
+	CounterManager();
+	~CounterManager();
+	bool check_number_counter(std::string name, int value);
+	bool check_text_counter(std::string name, std::string value);
+private:
+	std::map<std::string, int> number_counters; //all of your number varriable names and values
+	std::map<std::string, std::string> text_counters; //all of your text variable names and values
+	void create_number_counter(std::string name, int value); //creates a new number value
+	void create_sting_counter(std::string name, std::string value); //creates a text value
+
+
+};
+
