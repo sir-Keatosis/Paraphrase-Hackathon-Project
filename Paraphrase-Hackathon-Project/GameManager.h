a67fc99af2b38ce777ac44a2da6909e6a68f18fd
Seaney Shell <sfs185@humboldt.edu>
2020-04-04 23:06:13 -0700

    added a start function that asks for a file to start form or a file name to load from

--- Paraphrase-Hackathon-Project/GameManager.h
+++ Paraphrase-Hackathon-Project/GameManager.h
@@ -11,6 +11,7 @@ class GameManager
 		void start();
 		void setPortal(std::string fileName, std::string displayText);
 		void choosePortal();
+		std::string currentChapter;
 
 	private:
 		std::vector <Portal> portals;
