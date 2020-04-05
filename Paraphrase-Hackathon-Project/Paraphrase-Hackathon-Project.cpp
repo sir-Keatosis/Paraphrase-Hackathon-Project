253ffb41dfc587a0f9f69e498bb578fe1ba48532
brainsincluded <45725627+brainsincluded@users.noreply.github.com>
2020-04-04 17:31:11 -0700

    added text file reading capabilities

--- Paraphrase-Hackathon-Project/Paraphrase-Hackathon-Project.cpp
+++ Paraphrase-Hackathon-Project/Paraphrase-Hackathon-Project.cpp
@@ -4,21 +4,65 @@
 #include <iostream>
 #include "TextParser.h"
 #include "CounterManager.h"
+#include <cstdlib>
+#include <fstream>
+#include <string>
 
 void testCounter();
 void testTextParser();
+std::ifstream stream;
+std::string test;
+
+bool isRightType(std::string filePath)
+{
+
+	return (filePath.substr(filePath.length() - 4) == ".txt" ? true : false);
 
 
+}
+
+std::string readFile(std::ifstream& stream, std::string file)
+{
+	const std::string failStmnt = "Sorry, unable to open file";
+
+	if (isRightType(file) == true)
+	{
+		stream.open(file);
+		if (stream.good() == true)
+		{
+			{
+				std::string output = "";
+				while (!stream.eof())
+				{
+					std::getline(stream, output);
+				}
+				stream.close();
+				return output;
+			}
+		}
+		else
+		{
+			return failStmnt;
+		}
+
+
+	}
+	else
+	{
+		return failStmnt;
+	}
+}
 
 int main()
 {
 	testTextParser();
 	//
+	test = readFile(stream, "./textfiles/Text.txt");
 
 	// print something to the terminal
     std::cout << "Welcome to Paraphrase Text adventure tool!\n";
 	std::cout << (('5' - '0') + 1) << std::endl;	
-
+	std::cout << test << std::endl;
 	// puase for user input
 	std::cin.get();
 
